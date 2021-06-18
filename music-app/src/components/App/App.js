587f03488d6809ac6f5dc142ac9739d8b8694b4c
manan2110 <manan.co.in@gmail.com>
2021-06-18 23:56:27 +0530

    SearchBar component Added

--- music-app/src/components/App/App.js
+++ music-app/src/components/App/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+
+import Playlist from "../Playlist/Playlist";
+import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
+import Spotify from "../util/Spotify"
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      SearchResults: [],
+      playlistName: "New Playlist",
+      playlistTracks: []
+    };
+    this.search = this.search.bind(this);
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.removeTrackSearch = this.removeTrackSearch.bind(this);
+    this.doThese = this.doThese.bind(this);
+
+  }
+  search(term) {
+    Spotify.search(term).then(SearchResults => {
+      this.setState({ SearchResults: SearchResults })
+    });
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id == track.id)) {
+
+    }
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    let trackSearch = this.state.SearchResults;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+    trackSearch = unshift(track);
+    this.setState({ playlistTracks: track });
+  }
+
+  removeTrackSearch(track) {
+    let tracks = this.state.SearchResults;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id)
+    this.setState({ SearchResults: tracks });
+  }
+  doThese(track) {
+    this.addTrack(track);
+    this.removeTrackSearch(track);
+  }
+  updatePlaylistName(name) {
+    this.setState({ updatePlaylistName: name });
+  }
+  savePlaylist() {
+    const trackUrls = this.state.playlistTracks.map(track => track.url);
+    Spotify.savePlaylist(this.state.playlistName, trackUrls).then(() => {
+      this.setState({
+        updatePlaylistName: "New Playlist",
+        playlistTracks: []
+      });
+    });
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        <a href="http://localhost:3000">Musicophile</a>
+      </h1>
+      <div className="App">
+        <SearchBar onSearch={this.search} />
+        <div className="App-playlist">
+          <SearchResults searchResults={this.state.searchResults} onAdd={this.doThese} />
+          <Playlist playlistTracks={this.state.playlistTracks} onNameChange={this.updatePlaylistName} onRemove={this.removeTrack} onSave={this.savePlaylist} />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ music-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import "./SearcgBar.css"
+
+export class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ""
+        };
+        this.handelTermChange = this.handelTermChange.bind(this);
+        this.search = this.search.bind(this);
+        this.handelEnter = this.handelEnter.bind(this);
+    };
+    handelTermChange(event) {
+        this.setState({ term: event.target.value });
+    }
+
+    search() {
+        this.props.onSearch(this.state.term);
+    }
+    handelEnter(event) {
+        if (event.keyCode === 13) {
+            this.search();
+        }
+    }
+    render() {
+        return (
+            <div className="SearchBar">
+                <input placeholder="Enter Song , Album or Artist" onChange={this.handelTermChange} onKeyUp={this.handelEnter} />
+                <button className="SearchButton" onclick={this.search}>SEARCH</button>
+            </div>
+        );
+    }
+}
+
+export default SearchBar
