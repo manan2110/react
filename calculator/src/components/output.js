9f73044d3060a92ef647531d912e3a69c6de0f38
manan2110 <manan.co.in@gmail.com>
2021-06-19 14:33:06 +0530

    Made layout and Output screen

---
+++ calculator/src/components/output.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import OutputRow from './outputRow';
+const Output = props => {
+    return (
+        <div>
+            <OutputRow value={props.answer} textSize={{ fontSize: '20px' }}></OutputRow>
+            <OutputRow value={props.user} textSize={{ fontSize: '20px' }}></OutputRow>
+        </div>
+    )
+}
+
+export default Output;
