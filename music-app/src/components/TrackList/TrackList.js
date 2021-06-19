5cb6338394a85101d4746860dc99fe8a6aebd358
manan2110 <manan.co.in@gmail.com>
2021-06-19 10:25:02 +0530

    Fixed a error

--- music-app/src/components/TrackList/TrackList.js
+++ music-app/src/components/TrackList/TrackList.js
@@ -5,21 +5,32 @@ import Track from "../Track/Track"
 
 class TrackList extends React.Component {
     render() {
-        return (
-            <div className="TrackList">
-                {this.props.tracks.map(track => {
-                    return (
-                        <Track
-                            track={track}
-                            key={track.id}
-                            onAdd={this.props.onAdd}
-                            isRemoval={this.props.isRemoval}
-                            onRemove={this.props.onRemove}
-                        />
-                    );
-                })}
-            </div>
-        );
+        if (this.props.tracks) {
+            return (
+                <div className="TrackList">
+
+                    {this.props.tracks.map((track) => {
+                        return (
+                            <Track
+                                track={track}
+                                key={track.id}
+                                onAdd={this.props.onAdd}
+                                isRemoval={this.props.isRemoval}
+                                onRemove={this.props.onRemove}
+                            />
+                        );
+                    })}
+                </div>
+
+            );
+        }
+        else {
+            return (
+                <div className="TrackList">
+
+                </div>
+            );
+        }
     }
 }
 
