93707222148c1019fc7bd84cd9074ca3f6f1ca0a
manan2110 <manan.co.in@gmail.com>
2021-06-20 11:24:26 +0530

    Created component TodoForm

---
+++ todo-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TodoForm from './components/TodoForm';
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoForm></TodoForm>
+
+    </div>
+  );
+}
+
+export default App;
