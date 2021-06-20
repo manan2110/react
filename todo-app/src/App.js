4b6013dbb1989e77dc0b8613439a742d2aaa706c
manan2110 <manan.co.in@gmail.com>
2021-06-20 13:26:47 +0530

    created todo-list component

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 function App() {
   return (
     <div className="todo-app">
-      <TodoForm></TodoForm>
+      <TodoList />
 
     </div>
   );
