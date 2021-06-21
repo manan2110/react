ae6c244f66ffff638e49c33dbb40af7137767324
manan2110 <manan.co.in@gmail.com>
2021-06-21 18:17:24 +0530

    created function to remove and added react-icons

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import TodoForm from './TodoForm'
+import Todo from './Todo';
 function TodoList() {
     const [todos, setTodos] = useState([]);
     const addTodo = todo => {
@@ -10,10 +11,26 @@ function TodoList() {
         setTodos(newTodos)
         // console.log(todo, ...todos);
     };
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id)
+
+        setTodos(removeArr)
+    };
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete
+            }
+            return todo;
+        })
+        setTodos(updatedTodos);
+    };
     return (
         <div>
             <h1>What's the plan for today</h1>
             <TodoForm onSubmit={addTodo}></TodoForm>
+            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo}
+            />
         </div>
     );
 }
